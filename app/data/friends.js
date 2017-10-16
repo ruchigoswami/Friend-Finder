f81811afc4e50a20e56b29f0600073bde27b16b1
ruchigoswami <awasthiruchi.us@gmail.com>
2017-10-16 11:17:39 -0400

    'initial'

---
+++ app/data/friends.js
@@ -0,0 +1,35 @@
+//create array of default friends
+
+var friends = [{
+  name: "Aishwarya Rai",
+  photo: "https://i.pinimg.com/736x/b8/02/d2/b802d2da087f3d2e89210fb1aae41d94.jpg",
+  scores: [3, 1, 4, 4, 2, 4, 5, 2, 4, 2]
+}, {
+  name: "Deepika Padukon",
+  photo: "http://www.hindustantimes.com/rf/image_size_960x540/HT/p2/2017/03/24/Pictures/actor-deepika-padukone-photo-credit-prasad-naik_b4084092-1072-11e7-9d5b-3c373065cf85.jpg",
+  scores: [2, 5, 3, 4, 5, 3, 1, 3, 2, 4]
+}, {
+  name: "Alia Bhatt",
+  photo: "http://www.worldstopmost.com/wp-content/uploads/2016/07/Alia-Bhatt-Most-Beautiful-Indian-Actresses-2016.jpg",
+  scores: [4, 3, 3, 2, 1, 2, 5, 4, 2, 1]
+},{
+  name: "Kareena Kapoor",
+  photo: "http://3.bp.blogspot.com/-VN0a5NG7_9A/U6iNhWFvKcI/AAAAAAAARPY/_gaThlEP194/s1600/Top-10-beautiful-Indian-Actresses-and-Top-Ten-Bollywood-Actresses-in-Bollywood-Film-industry-funfashion1.com+Kareena+Kapoor+Khan+2.jpg",
+  scores: [1, 2, 3, 3, 4, 1, 2, 5, 3, 3]
+}, {
+  name: "Priyanka Chopra",
+  photo: "https://images-na.ssl-images-amazon.com/images/M/MV5BMjAxNzUwNjExOV5BMl5BanBnXkFtZTcwNDUyMTUxNw@@._V1_SY1000_CR0,0,1339,1000_AL_.jpg",
+  scores: [5, 4, 5, 3, 2, 4, 2, 1, 3, 2]
+}, {
+  name: "Kaitreena Kaif",
+  photo: "https://hdwallpapersbuzz.com/wp-content/uploads/2017/02/katrina-kaif-hd-wallpapers-images-45.jpg",
+  scores: [2, 1, 1, 2, 5, 4, 3, 3, 1, 4]
+}, {
+  name: "Karishma Kapoor",
+  photo: "http://www.dailyexcelsior.com/wp-content/uploads/2013/09/karishma-kapoor-48w.jpg",
+  scores: [2, 2, 1, 3, 4, 2, 5, 3, 3, 4]
+}];
+
+//-------export array----------
+
+module.exports = friends;
